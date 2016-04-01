cacedc83b5290c8f3e6bcb117890dceff42bc81f
igosja <igosja@mail.ru>
2016-04-01 17:50:10 +0300

    Раскомментил подсчет количества запросов к БД при генерации страницы.

--- generator/function/f_igosja_mysqli_query.php
+++ generator/function/f_igosja_mysqli_query.php
@@ -3,13 +3,13 @@
 function f_igosja_mysqli_query($sql)
 //Логируем запросы (На деве нужно расскомментировать все строки)
 {
-//    global $count_sql;
+    global $count_sql;
     global $mysqli;
 
 //    $start_time = microtime(true);
     $result_sql = $mysqli->query($sql);
 //    $query_time = round((microtime(true) - $start_time) * 1000, 2);
-//    $count_sql++;
+    $count_sql++;
 //    $text = addslashes($sql);
 
 //    $sql = "INSERT INTO `debug`
