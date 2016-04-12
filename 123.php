31149a09d9d74c3223f8063dc85adf2b4ad8be6a
igosja <igosja@mail.ru>
2016-04-12 16:48:53 +0300

    +

--- 123.php
+++ 123.php
@@ -2,10 +2,5 @@
 
 include ($_SERVER['DOCUMENT_ROOT'] . '/include/include.php');
 
-$sql = "UPDATE `country`
-        SET `country_user_id`='0'
-        WHERE `country_user_id`!='0'";
-$mysqli->query($sql);
-
 print '<br/>Страница сгенерирована за ' . round(microtime(true) - $start_time, 5) . ' сек. в ' . date('H:i:s') . '
        <br/>Потребление памяти: ' . number_format(memory_get_usage(), 0, ",", " ") . ' Б';
