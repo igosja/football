2c85abc7df0da5e9f7bd56e916f89b1f70548dd5
Igosja <igosja@ukr.net>
2016-04-20 22:46:58 +0300

    Шаблон "победители ЛЧ"

--- view/league_history_winner.php
+++ view/league_history_winner.php
@@ -41,39 +41,6 @@
                         </td>
                     </tr>
                 <?php } ?>
-                <?php foreach ($winner_array as $item) { ?>
-                    <tr>
-                        <td class="center"><?= $item['shedule_season_id']; ?></td>
-                        <td class="w1">
-                            <img
-                                alt="<?= $item['winner_name']; ?>"
-                                class="img-12"
-                                src="/img/team/12/<?= $item['winner_id']; ?>.png"
-                            />
-                        </td>
-                        <td class="w40">
-                            <a href="team_team_review_profile.php?num=<?= $item['winner_id']; ?>">
-                                <?= $item['winner_name']; ?>
-                                (<?= $item['winner_city']; ?>,
-                                <?= $item['winner_country']; ?>)
-                            </a>
-                        </td>
-                        <td class="w1">
-                            <img
-                                alt="<?= $item['looser_name']; ?>"
-                                class="img-12"
-                                src="/img/team/12/<?= $item['looser_id']; ?>.png"
-                            />
-                        </td>
-                        <td class="w40">
-                            <a href="team_team_review_profile.php?num=<?= $item['looser_id']; ?>">
-                                <?= $item['looser_name']; ?>
-                                (<?= $item['looser_city']; ?>,
-                                <?= $item['looser_country']; ?>)
-                            </a>
-                        </td>
-                    </tr>
-                <?php } ?>
             </table>
         </td>
     </tr>
