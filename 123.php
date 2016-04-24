eeaf46e2633bca1421d4c27cf4a5906df781de55
Igor Boyko <igosja@mail.ru>
2016-04-24 16:55:56 +0300

    Рассылка

--- 123.php
+++ 123.php
@@ -2,13 +2,25 @@
 
 include (__DIR__ . '/include/include.php');
 
-$email   = 'igosja@ukr.net';
-$subject = 'Запуск сайта ' . $_SERVER['HTTP_HOST'];
-$message = 'Здравствуйте, Игося.<br/><br/>Рады сообщить вам, что сайт <a href="http://' . $_SERVER['HTTP_HOST'] . '">' . $_SERVER['HTTP_HOST'] . '</a> начинает свою работу, вы можете войти на сайт используя свой логин и пароль и взять под свое управление поднравившуюся команду.<br/><br/>С уважением,<br/>администрация <a href="http://' . $_SERVER['HTTP_HOST'] . '">' . $_SERVER['HTTP_HOST'] . '</a>';
-$header  = "From: <noreply@" . $_SERVER['HTTP_HOST'] . ">\r\n";
-$header .= "Content-type:text/html; charset=utf-8\r\n";
+$sql = "SELECT `user_email`,
+               `user_login`
+        FROM `user`
+        WHERE `user_id`>'0'";
+$user_sql = $mysqli->query($sql);
 
-mail($email, $subject, $message, $header);
+$user_array = $user_sql->fetch_all(MYSQLI_ASSOC);
+
+foreach ($user_array as $user)
+{
+    $email      = $user['user_email'];
+    $login      = $user['user_login'];
+    $subject    = 'Запуск сайта ' . $_SERVER['HTTP_HOST'];
+    $message    = 'Здравствуйте, ' . $login . '.<br/><br/>Рады сообщить вам, что сайт <a href="http://' . $_SERVER['HTTP_HOST'] . '">' . $_SERVER['HTTP_HOST'] . '</a> начинает свою работу, вы можете войти на сайт используя свой логин и пароль и взять под свое управление понравившуюся команду.<br/><br/>С уважением,<br/>администрация <a href="http://' . $_SERVER['HTTP_HOST'] . '">' . $_SERVER['HTTP_HOST'] . '</a>';
+    $header     = "From: <admin@" . $_SERVER['HTTP_HOST'] . ">\r\n";
+    $header     = $header . "Content-type:text/html; charset=utf-8\r\n";
+
+    mail($email, $subject, $message, $header);
+}
 
 print '<br />Страница сгенерирована за ' . round(microtime(true) - $start_time, 5) . ' сек. в ' . date('H:i:s') . '
        <br />Потребление памяти: ' . number_format(memory_get_usage(), 0, ",", " ") . ' Б';
