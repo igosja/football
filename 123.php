560b203e3a11589b4fc6ab63b0522a4f3b40c764
Igor Boyko <igosja@mail.ru>
2016-04-24 17:03:09 +0300

    +

--- 123.php
+++ 123.php
@@ -2,25 +2,7 @@
 
 include (__DIR__ . '/include/include.php');
 
-$sql = "SELECT `user_email`,
-               `user_login`
-        FROM `user`
-        WHERE `user_id`='1'";
-$user_sql = $mysqli->query($sql);
-
-$user_array = $user_sql->fetch_all(MYSQLI_ASSOC);
-
-foreach ($user_array as $user)
-{
-    $mail_to    = $user['user_email'];
-    $login      = $user['user_login'];
-    $subject    = 'Запуск сайта ' . $_SERVER['HTTP_HOST'];
-    $message    = 'Здравствуйте, ' . $login . '.<br/><br/>Рады сообщить вам, что сайт <a href="http://' . $_SERVER['HTTP_HOST'] . '">' . $_SERVER['HTTP_HOST'] . '</a> начинает свою работу, вы можете войти на сайт используя свой логин и пароль и взять под свое управление понравившуюся команду.<br/><br/>С уважением,<br/>администрация <a href="http://' . $_SERVER['HTTP_HOST'] . '">' . $_SERVER['HTTP_HOST'] . '</a>';
-    $header     = "From: <admin@" . $_SERVER['HTTP_HOST'] . ">\r\n";
-    $header     = $header . "Content-type:text/html; charset=utf-8\r\n";
-
-    mail($mail_to, $subject, $message, $header);
-}
+print 1;
 
 print '<br />Страница сгенерирована за ' . round(microtime(true) - $start_time, 5) . ' сек. в ' . date('H:i:s') . '
        <br />Потребление памяти: ' . number_format(memory_get_usage(), 0, ",", " ") . ' Б';
