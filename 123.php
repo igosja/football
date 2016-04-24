5c058b670b58c4a945da29ef5579daa42f9040f3
Igor Boyko <igosja@mail.ru>
2016-04-24 16:50:37 +0300

    +

--- 123.php
+++ 123.php
@@ -4,7 +4,7 @@ include (__DIR__ . '/include/include.php');
 
 $email   = 'igosja@ukr.net';
 $subject = 'Запуск сайта ' . $_SERVER['HTTP_HOST'];
-$message = 'Здравствуйте, Игося.<br/><br/>Рады сообщить вам, что сайт <a href="http://' . $_SERVER['HTTP_HOST'] . '>' . $_SERVER['HTTP_HOST'] . '</a> начинает свою работу, вы можете войти на сайт используя свой логин и пароль и взять под свое управление поднравившуюся команду.<br/><br/>С уважением,<br/>администрация <a href="http://' . $_SERVER['HTTP_HOST'] . '>' . $_SERVER['HTTP_HOST'] . '</a>';
+$message = 'Здравствуйте, Игося.<br/><br/>Рады сообщить вам, что сайт <a href="http://' . $_SERVER['HTTP_HOST'] . '">' . $_SERVER['HTTP_HOST'] . '</a> начинает свою работу, вы можете войти на сайт используя свой логин и пароль и взять под свое управление поднравившуюся команду.<br/><br/>С уважением,<br/>администрация <a href="http://' . $_SERVER['HTTP_HOST'] . '">' . $_SERVER['HTTP_HOST'] . '</a>';
 $header  = "From: <noreply@" . $_SERVER['HTTP_HOST'] . ">\r\n";
 $header .= "Content-type:text/html; charset=utf-8\r\n";
 
