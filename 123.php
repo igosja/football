4e9a427c6ac82c6ee7c938959ba254cc8a12e6d2
Igor Boyko <igosja@mail.ru>
2016-04-24 16:47:42 +0300

    +

--- 123.php
+++ 123.php
@@ -5,8 +5,8 @@ include (__DIR__ . '/include/include.php');
 $email   = 'igosja@ukr.net';
 $subject = 'Запуск сайта ' . $_SERVER['HTTP_HOST'];
 $message = 'Здравствуйте, Игося.<br/>Рады сообщить вам, что сайт ' . $_SERVER['HTTP_HOST'] . ' начинает свою работу, вы можете войти на сайт используя свой логин и пароль и взять под свое управление поднравившуюся команду.<br/>С уважением, администрация ' . $_SERVER['HTTP_HOST'];
-$header  = 'From: <admin@' . $_SERVER['HTTP_HOST'] . '>\r\n';
-$header .= 'Content-type:text/html; charset=utf-8\r\n';
+$header  = "From: <noreply@" . $_SERVER['HTTP_HOST'] . ">\r\n";
+$header .= "Content-type:text/html; charset=utf-8\r\n";
 
 mail($email, $subject, $message, $header);
 
