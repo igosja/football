cf7b39b30813f9b422004f436938c259ad7ab5e6
igosja <igosja@mail.ru>
2016-04-25 10:40:50 +0300

    Обновил главную в админке - удалил лишнее.

--- admin/user_list.php
+++ admin/user_list.php
@@ -2,8 +2,11 @@
 
 include ($_SERVER['DOCUMENT_ROOT'] . '/include/include.php');
 
-$sql = "SELECT `user_id`, `user_last_visit`, `user_login`
+$sql = "SELECT `user_id`,
+               `user_last_visit`,
+               `user_login`
         FROM `user`
+        WHERE `user_id`!='0'
         ORDER BY `user_last_visit` DESC";
 $user_sql = $mysqli->query($sql);
 
