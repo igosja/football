a44b01d3259a11abbc42277c69acf258775e7fab
Igor Boyko <igosja@mail.ru>
2016-04-25 21:20:08 +0300

    +

--- view/include/footer.php
+++ view/include/footer.php
@@ -7,7 +7,7 @@
         <p>Страница сгенерирована за <?= round(microtime(true) - $start_time, 5); ?> сек.<p>
         <p>Потребление памяти: <?= number_format(memory_get_usage(), 0, ",", " "); ?> Б</p>
         <p>Версия сайта: <?= $site_array[0]['site_version_1'] ?>.<?= $site_array[0]['site_version_2'] ?>.<?= $site_array[0]['site_version_3'] ?>.<?= $site_array[0]['site_version_4'] ?> от <?= date('d.m.Y', $site_array[0]['site_version_date']); ?></p>
-        <?php if (1 < $authorization_permission) { ?>
+        <?php if (1 < $authorization_permission && 1 == 0) { ?>
             <p><a href="admin">Административный раздел</a></p>
         <?php } ?>
     </div>
