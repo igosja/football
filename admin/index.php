cacddb7dd221d640ae05785e2728f67c7cbe6051
igosja <igosja@mail.ru>
2016-04-27 10:59:13 +0300

    Вывод количества свободных команд на главной в админке.

--- admin/index.php
+++ admin/index.php
@@ -2,4 +2,13 @@
 
 include ($_SERVER['DOCUMENT_ROOT'] . '/include/include.php');
 
+$sql = "SELECT COUNT(`team_id`) AS `count`
+        FROM `team`
+        WHERE `team_id`!='0'
+        AND `team_user_id`='0'";
+$freeteam_sql = $mysqli->query($sql);
+
+$freeteam_array = $freeteam_sql->fetch_all(MYSQLI_ASSOC);
+$count_freeteam = $freeteam_array[0]['count'];
+
 include ($_SERVER['DOCUMENT_ROOT'] . '/view/admin_main.php');
