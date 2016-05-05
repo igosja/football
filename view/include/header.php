22347cc66b3fe4e8d1612c08d2444a9b10964ad5
igosja <igosja@mail.ru>
2016-05-05 09:59:24 +0300

    Яндекс верификация

--- view/include/header.php
+++ view/include/header.php
@@ -6,6 +6,7 @@
     <meta name="description" content="<?= $seo_description; ?>" />
     <meta name="keywords" content="<?= $seo_keywords; ?>" />
     <meta charset="utf-8" />
+    <meta name='yandex-verification' content='459212417818131c' />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="/css/style.css">
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
