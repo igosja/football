4795e9c25e24156b63670eb3fb9c1cf70a2c4eda
igosja <igosja@mail.ru>
2016-06-21 13:04:31 +0300

    Кто онлайн на форуме

--- view/forum_theme.php
+++ view/forum_theme.php
@@ -111,7 +111,7 @@
                                 </a>
                                 <br />
                             <?php } ?>
-                            <?= f_igosja_ufu_date_time($forum_array[$i]['user_last_visit']); ?>
+                            <?= f_igosja_ufu_last_visit_forum($forum_array[$i]['user_last_visit']); ?>
                             <?php if (isset($authorization_id) && $authorization_id != $forum_array[$i]['user_id']) { ?>
                                 <a href="profile_news_outbox.php?answer=<?= $forum_array[$i]['user_id']; ?>" class="button-link">
                                     <button>
