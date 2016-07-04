69acf3c751e281d81a58fdbbf3b394b14ddffe5a
igosja <igosja@mail.ru>
2016-07-04 15:05:46 +0300

    freekassa

--- shop.php
+++ shop.php
@@ -243,6 +243,7 @@ elseif (isset($_POST['data']))
         'oa'    => $sum,
         'o'     => $mysqli->insert_id,
         's'     => md5($merchant_id . ':' . $sum . ':' . $secret_key . ':' . $order_id),
+        'i'     => 1,
         'lang'  => 'ru',
     );
 
